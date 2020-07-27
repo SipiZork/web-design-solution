97e86f7c55736b80be2f6cb4d0524758add0afb9
Mark <sipizork@gmail.com>
2020-07-27 13:13:07 +0200

    first commit

---
+++ about-us.js
@@ -0,0 +1,51 @@
+const us = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fermentum dolor eget ex auctor accumsan. Quisque luctus tellus ipsum, vel pretium est commodo non.Nulla sodales cursus ullamcorper. Donec lectus massa, lobortis nec ante vel, scelerisque tristique sapien. Donec non dapibus sapien. Fusce eget sem efficitur velit fermentum finibus cursus nec est. Proin aliquet porta vehicula.</p><p>Nulla in elit purus. Suspendisse tincidunt ex in tellus consectetur, at sollicitudin urna blandit. Nam arcu erat, efficitur a neque nec, elementum congue felis. Maecenas sodales ligula a quam venenatis pharetra. Suspendisse potenti. Ut sodales, sapien non scelerisque tincidunt, sem nisi laoreet sapien, et interdum quam nibh vel velit. Phasellus tempor tincidunt elit nec pellentesque. Nunc vitae enim ex. Nunc turpis elit, vestibulum congue erat id, ultrices malesuada dolor. Suspendisse congue sed nulla blandit commodo. Sed aliquet, nulla quis tempor euismod, risus leo blandit lacus, in lobortis nibh justo at libero.</p>';
+const company = '<p>Ut sem erat, auctor non metus nec, volutpat convallis nunc. Vestibulum et porttitor massa. In tortor turpis, tincidunt at lorem ac, aliquam consequat mi. Aenean non consequat sapien. Nulla ultricies dignissim quam quis viverra. Vivamus quis finibus mauris, at gravida lorem. Ut quis sagittis massa. Suspendisse placerat dolor magna, eget cursus risus convallis sed. Cras in neque lorem. In dictum, dui et maximus facilisis, dui ante vulputate lorem, a accumsan nulla leo ut tellus. Nullam convallis sem a leo vestibulum, ac mollis eros varius.</p><p>Nam pellentesque consequat sollicitudin. Donec condimentum dui velit, vitae interdum dui consequat fringilla. Donec pellentesque rhoncus odio, elementum placerat quam hendrerit et. Aliquam tincidunt vitae est in gravida. Phasellus non sapien lobortis, blandit nisi in, facilisis enim. Sed ligula orci, imperdiet nec erat gravida, vulputate gravida neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed et dolor ultricies, gravida nulla nec, laoreet lacus. Duis ultrices, odio sit amet scelerisque pulvinar, lorem arcu convallis sem, quis facilisis turpis mauris nec velit. Proin quis tincidunt magna, sed placerat lectus. Aliquam quis urna ante. Sed vel congue ipsum, id euismod felis.</p>';
+const goals = '<p>Nam pellentesque consequat sollicitudin. Donec condimentum dui velit, vitae interdum dui consequat fringilla. Donec pellentesque rhoncus odio, elementum placerat quam hendrerit et. Aliquam tincidunt vitae est in gravida. Phasellus non sapien lobortis, blandit nisi in, facilisis enim. Sed ligula orci, imperdiet nec erat gravida, vulputate gravida neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed et dolor ultricies, gravida nulla nec, laoreet lacus. Duis ultrices, odio sit amet scelerisque pulvinar, lorem arcu convallis sem, quis facilisis turpis mauris nec velit. Proin quis tincidunt magna, sed placerat lectus. Aliquam quis urna ante. Sed vel congue ipsum, id euismod felis.</p><p>Vivamus nec mauris et nulla dictum convallis quis nec ante. Donec nec euismod eros. Phasellus id venenatis felis, id hendrerit erat. Sed et venenatis erat. Etiam eleifend dapibus enim sit amet cursus. Mauris facilisis, dui nec tempor pharetra, ex nunc maximus sapien, in cursus elit lacus sed lorem. Vestibulum ligula neque, fringilla eu velit ut, aliquam ultrices elit. Quisque nec porttitor massa. Quisque mauris nisl, pretium fringilla convallis in, pellentesque ac libero. Nunc finibus sollicitudin magna, at facilisis lectus. Cras bibendum arcu a neque mollis ultricies. Sed porta magna vitae bibendum dignissim. Mauris pharetra blandit nisi ut aliquam. Integer vestibulum metus velit, sed feugiat magna rhoncus vel.</p>'
+const motivation = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fermentum dolor eget ex auctor accumsan. Quisque luctus tellus ipsum, vel pretium est commodo non. Nulla sodales cursus ullamcorper. Donec lectus massa, lobortis nec ante vel, scelerisque tristique sapien. Donec non dapibus sapien. Fusce eget sem efficitur velit fermentum finibus cursus nec est. Proin aliquet porta vehicula.</p>Nulla in elit purus. Suspendisse tincidunt ex in tellus consectetur, at sollicitudin urna blandit. Nam arcu erat, efficitur a neque nec, elementum congue felis. Maecenas sodales ligula a quam venenatis pharetra. Suspendisse potenti. Ut sodales, sapien non scelerisque tincidunt, sem nisi laoreet sapien, et interdum quam nibh vel velit. Phasellus tempor tincidunt elit nec pellentesque. Nunc vitae enim ex. Nunc turpis elit, vestibulum congue erat id, ultrices malesuada dolor. Suspendisse congue sed nulla blandit commodo. Sed aliquet, nulla quis tempor euismod, risus leo blandit lacus, in lobortis nibh justo at libero.<p></p><p>Ut sem erat, auctor non metus nec, volutpat convallis nunc. Vestibulum et porttitor massa. In tortor turpis, tincidunt at lorem ac, aliquam consequat mi. Aenean non consequat sapien. Nulla ultricies dignissim quam quis viverra. Vivamus quis finibus mauris, at gravida lorem. Ut quis sagittis massa. Suspendisse placerat dolor magna, eget cursus risus convallis sed. Cras in neque lorem. In dictum, dui et maximus facilisis, dui ante vulputate lorem, a accumsan nulla leo ut tellus. Nullam convallis sem a leo vestibulum, ac mollis eros varius.</p><p>Nam pellentesque consequat sollicitudin. Donec condimentum dui velit, vitae interdum dui consequat fringilla. Donec pellentesque rhoncus odio, elementum placerat quam hendrerit et. Aliquam tincidunt vitae est in gravida. Phasellus non sapien lobortis, blandit nisi in, facilisis enim. Sed ligula orci, imperdiet nec erat gravida, vulputate gravida neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed et dolor ultricies, gravida nulla nec, laoreet lacus. Duis ultrices, odio sit amet scelerisque pulvinar, lorem arcu convallis sem, quis facilisis turpis mauris nec velit. Proin quis tincidunt magna, sed placerat lectus. Aliquam quis urna ante. Sed vel congue ipsum, id euismod felis.</p><p>Vivamus nec mauris et nulla dictum convallis quis nec ante. Donec nec euismod eros. Phasellus id venenatis felis, id hendrerit erat. Sed et venenatis erat. Etiam eleifend dapibus enim sit amet cursus. Mauris facilisis, dui nec tempor pharetra, ex nunc maximus sapien, in cursus elit lacus sed lorem. Vestibulum ligula neque, fringilla eu velit ut, aliquam ultrices elit. Quisque nec porttitor massa. Quisque mauris nisl, pretium fringilla convallis in, pellentesque ac libero. Nunc finibus sollicitudin magna, at facilisis lectus. Cras bibendum arcu a neque mollis ultricies. Sed porta magna vitae bibendum dignissim. Mauris pharetra blandit nisi ut aliquam. Integer vestibulum metus velit, sed feugiat magna rhoncus vel.</p>';
+
+const links = document.querySelectorAll('.link');
+const content = document.querySelector('.right--content');
+let loadedContent = 'us'; 
+content.innerHTML = motivation;
+let load = false;
+
+const loadContent = (clickedLink, queryContent) => {
+  if (!load) {
+    load = true;
+    links.forEach(link => {
+      link.classList.remove('active');
+      clickedLink.classList.add('active');
+      content.style.opacity = 0;
+    });
+    setTimeout(() => {
+      if (queryContent !== loadedContent) {
+        loadedContent = queryContent;
+        switch (queryContent) {
+          case 'us':
+            content.innerHTML = us;
+            break;
+          case 'company':
+            content.innerHTML = company;
+            break;
+          case 'goals':
+            content.innerHTML = goals;
+            break;
+          case 'motivation':
+            content.innerHTML = motivation;
+            break;
+        }
+        
+      }
+    }, 300);
+    setTimeout(() => {
+      content.style.opacity = 1;
+    }, 350);
+    setTimeout(() => {
+      load = false;
+    }, 650);
+  }
+}
+
+links.forEach(link => {
+  link.addEventListener('click', () => loadContent(link, link.dataset.content));
+});
